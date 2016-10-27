2730b40750e23a284a35290acac6fefaa1481259
sobolrr <sobolroman22@gmail.com>
2016-10-27 10:20:30 +0300

    added pie chart

--- companies/companies.ctrl.js
+++ companies/companies.ctrl.js
@@ -5,17 +5,73 @@ angular
 .module('myApp.companies')
 .controller('companies.ctrl', Controller);
 function Controller($scope, $http) {
-	//$scope.countCompany = 0;
-	let init = function() {
-		$http.get('http://codeit.pro/frontTestTask/company/getList')
-		.success(function(data) {
-			console.log(data);
-			$scope.countCompany = data.list.length;
-			console.log(data.list.length);
-			$scope.companies = data.list;
-			console.log(data.list);
-			})
+	const countriesMap = new Map();
+	const arrayColorForChart = [];
+
+	function init () {
+	$http.get('http://codeit.pro/frontTestTask/company/getList')
+	.success(function(data) {
+		$scope.countCompany = data.list.length;
+		$scope.companies = data.list;
+		makeCountriesMap(data.list);
+		makeArrayColor(countriesMap.size);
+		drawChart(Array.from(countriesMap.keys()), Array.from(countriesMap.values()), arrayColorForChart);
+		})
+	};
+	init();
+
+	function drawChart (labels, data, backgroundColor) {
+		var ctx = document.getElementById("myChart");
+		var data = {
+		    labels: labels,
+	    	datasets: [
+	        	{
+	            data: [34, 18, 23, 4, 18, 3],
+	            backgroundColor: backgroundColor,
+	        }]
 		};
-		init();
+			
+		$scope.myPieChart = new Chart(ctx,{
+			type: 'pie',
+			data: data,
+		    options: {
+				scales: {
+	    			yAxes: [{
+	        			ticks: {
+	            			beginAtZero:true
+	        			}
+	    			}]
+				}
+			}
+		});
+	}
+
+	function getRandomColor() {
+    	var letters = '0123456789ABCDEF'.split('');
+    	var color = '#';
+    	for (var i = 0; i < 6; i++ ) {
+       		color += letters[Math.floor(Math.random() * 16)];
+    	}
+    	return color;
+	}
+
+	function makeCountriesMap (countries) {
+		for(let i = 0; i < countries.length; i++) {
+			var temp = countries[i].location.name;
+			if(countriesMap.has(temp)){
+				var countCountry  = countriesMap.get(temp);
+				countCountry++;
+				countriesMap.set(temp, countCountry);
+			} else {
+				countriesMap.set(temp, 1);
+			}
+		}
+	}
+
+	function makeArrayColor(length) {
+		for(let i = 0; i < length; i++) {
+			arrayColorForChart[i] = getRandomColor();
+		}
+	}
 }
 })();
